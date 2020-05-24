3ff22befc8bac35b51ca0ec04ec59faff63a54e1
Alexander Fedorov <31668662+FArekkusu@users.noreply.github.com>
2020-05-24 18:49:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from laplacian_sobel import laplacian, sobel
 if __name__ == "__main__":
     functions = [
         gray_level_transformation,
-        min_max_mean_filter,
+        min_max_median_filter,
         laplacian,
         sobel
     ]
